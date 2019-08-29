0c544a16afb16a6dc70fcd313c1490d04fb3f1f2
joshua-miles <joshua@christetulit.org>
2019-08-29 10:51:48 -0500

    -

--- open.sh
+++ open.sh
@@ -4,5 +4,5 @@ unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     xdg-open "http://localhost:8080";;
     Darwin*)    open "http://localhost:8080";;
-    *)          echo "HERE3";;
+    *)          echo "Platform not supported. Please contact joshua@christetulit.org with your platform ID: ${unameOut}";;
 esac
